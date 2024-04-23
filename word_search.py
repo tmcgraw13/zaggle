106c37b4411e8af8d7f53ea068abd49aac285e2e
mccormackkm <kevjm19@vt.edu>
2024-04-22 22:08:55 -0400

    Adding word validation and scrabble dictionary

---
+++ word_search.py
@@ -0,0 +1,14 @@
+word_list = []
+with open("Collins Scrabble Words (2019).txt","r") as f:
+    word_list = f.readlines()[2:]
+    word_list = [x.rstrip("\n") for x in word_list]
+    word_list = [x for x in word_list if (len(x) >= 3 and len(x) <= 7) ]
+
+print(len(word_list))
+
+while True:
+    my_word = input("Enter a word: ").upper()
+    if my_word.isalpha():
+        print(my_word in word_list)
+    else:
+        print("Word must not contain numbers or special characters!")
