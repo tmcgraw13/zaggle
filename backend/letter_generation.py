32c46303f832df1ccefb5fab5568dcda7439314d
millerandy77 <firebolt.andy@gmail.com>
2024-06-04 21:05:58 -0400

    self.letters moved to init

--- backend/letter_generation.py
+++ backend/letter_generation.py
@@ -4,10 +4,11 @@ class LetterGeneration:
     # Methods - generates original tileset. 
     #         - gives player new letters_sequence.
     # Fields - 
-    letters_sequence = []
 
     def __init__(self):
         self.scrabble_bag = ['a','a','a','a','a','a','a','a','a','b','b','c','c','d','d','d','d','e','e','e','e','e','e','e','e','e','e','e','e','f','f','g','g','g','h','h','i','i','i','i','i','i','i','i','i','j','k','l','l','l','l','m','m','n','n','n','n','n','n','o','o','o','o','o','o','o','o','p','p','q','r','r','r','r','r','r','s','s','s','s','t','t','t','t','t','t','u','u','u','u','v','v','w','w','x','y','y','z','*','?']
+        
+        self.letters_sequence = []
 
     def gen_n_letters(self,n=500):
         
