893f42f3228c31796f7aad809d03c03fc294b138
“tmcgraw13” <Timothy.g.mcgraw@gmail.com>
2024-12-06 22:27:50 -0500

    consistent button in player name modal and added fun character gifs

--- frontend/src/components/ButtonStandard.tsx
+++ frontend/src/components/ButtonStandard.tsx
@@ -1,18 +1,21 @@
 interface ButtonProps {
-  onButtonClick: () => void;
+  onButtonClick?: () => void;
   buttonName: string;
   className?: string;  // Added className as an optional property
   disabled?: boolean;
+  type?: "button" | "submit" | "reset";  // type is optional
 }
 
 const ButtonStandard: React.FC<ButtonProps> = ({
   onButtonClick,
   buttonName,
   className = "",  // Default to an empty string if no className is provided
-  disabled = false // Defaut to NOT disabled
+  disabled = false, // Defaut to NOT disabled
+  type = "button"
 }) => {
   return (
     <button
+      type={type}
       id="myButton"
       onClick={onButtonClick}
       className={`px-8 py-4 text-xl font-semibold bg-blue-600 text-white rounded-lg shadow-lg transition-all duration-300 hover:bg-blue-700 hover:scale-105 transform ${className}`} // Made button bigger and added hover effects
