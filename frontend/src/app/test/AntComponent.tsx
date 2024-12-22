6775c2e30f9c28943b97172654e5c99eba1935f9
“tmcgraw13” <Timothy.g.mcgraw@gmail.com>
2024-12-22 16:14:51 -0500

    initial ant

--- frontend/src/app/test/AntComponent.tsx
+++ frontend/src/app/test/AntComponent.tsx
@@ -8,12 +8,16 @@ const AntComponent = () => {
 
   //Draw grid function
   const drawGrid = (ctx:CanvasRenderingContext2D, grid:any, squareSize:number)=>{
-    grid.forEach((row,i) =>{
-      row.forEach( (cell,j) =>{
+    grid.forEach((row:any,i:number) =>{
+      row.forEach( (cell:any,j:number) =>{
         //cell contains the actual grid value
-   
         //TODO fill each cell as white or black depending on cell value
-        
+        if(cell === 0){
+          ctx.strokeRect(i*squareSize,j*squareSize,squareSize,squareSize)
+        }else{
+          ctx.fillStyle = 'black'
+          ctx.fillRect(i*squareSize,j*squareSize,squareSize,squareSize)
+        }
         
       })
     })
@@ -25,12 +29,12 @@ const AntComponent = () => {
     
     ctx.fillStyle = "red"
     //TODO draw ant as a red square
+    ctx.fillRect(antPos.x*squareSize, antPos.y*squareSize,squareSize, squareSize)
      
   };
 
   //Draw grid function
   const render = (ctx:CanvasRenderingContext2D,ant:any,squareSize:number)=>{
-    
     ctx.clearRect(0,0,1000,1000);
     ant.update();
     drawGrid(ctx,ant.grid,squareSize);
@@ -53,34 +57,54 @@ const AntComponent = () => {
     }
 
     private rotateCW(){
-
-      //TODO rotate ant clockwise
+      this.direction = (this.direction + 1) % 4;
       return;
 
     }
 
     private rotateCCW(){
-
-      //TODO Rotate ant counter-clockwise
+      this.direction = (this.direction + 3) % 4;
       return;
 
     }
 
     public update(){
-
+      //  if(this.x < 0){
+      //   this.x = 
+      //  } 
       
       if (this.x<0 || this.y <0 || this.x>= this.grid[0].length || this.y >= this.grid[0][0].length){
-        //outside grid, do nothing
+        
         return;
       }
 
       let currSquare = this.grid[this.x][this.y]
 
-
       //TODO Flip square and rotate ant based on square color
+      if(currSquare == 1){
+        this.rotateCCW()
+        this.grid[this.x][this.y] = 0
+            }
+      else{
+        this.rotateCW()
+        this.grid[this.x][this.y] = 1
+      }
 
       //TODO Move forward based on ant's direction
-
+      switch(this.direction) {
+        case 0:
+          this.y -= 1
+          break;
+        case 1:
+          this.x += 1
+          break;
+        case 2:
+          this.y += 1
+          break;
+        default:
+          this.x -= 1
+          break;
+      }
 
       
     }
@@ -88,14 +112,12 @@ const AntComponent = () => {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    
 
     if (canvas && !canvasTransferred.current) {
       canvasTransferred.current = true;
       const ctx = canvas.getContext("2d")
       if (ctx != null){
-        
-
+        canvas.style.backgroundColor = 'white'
         // set some dimensions
         canvas.width = 1000;
         canvas.height = 1000;
